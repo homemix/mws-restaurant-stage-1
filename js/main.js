2e77c23103939fdf2647fad889887984e1738c2f
kennedy wambua <kenmutati.gmail.com>
2018-08-15 16:20:46 +0300

    feat:add responsive images

--- js/main.js
+++ js/main.js
@@ -161,6 +161,9 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.srcset = DBHelper.imageSrcsetForIndex(restaurant);
+  image.sizes = "300px";
+  //image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
   const name = document.createElement('h1');
