63bef8612a6cae3def6171fcae27b537956be982
homemix <39698005+homemix@users.noreply.github.com>
2018-08-15 21:10:07 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
-let cacheName = 'v1'; 
+let staticCacheName = 'v1';
 
-
-let cacheFiles = [
-                 './index.html',
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches.open(staticCacheName).then(function(cache) {
+			return cache.addAll([
+		'./index.html',
                 './',
                 './restaurant.html',
                 './js/dbhelper.js',
@@ -24,97 +26,32 @@ let cacheFiles = [
                './img/9.jpg',
                './img/10.jpg',
                'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css'
-				
-]
-
-self.addEventListener('install', function(e) {
-    console.log('service worker installed');
-
-   
-    e.waitUntil(
-
-     
-      caches.open(cacheName).then(function(cache) {
-
-        
-      console.log('service worker caching cachefiles');
-      return cache.addAll(cacheFiles);
-      })
-  ); 
+			]);
+		})
+	);
 });
 
+self.addEventListener('activate', function(event) {
+	event.waitUntil(
+		caches.keys()
+		.then(function(cacheNames) {
+			return Promise.all(
+				cacheNames.filter(function(cacheName) {
+					return cacheName.startsWith('restaurant-') &&
+						   cacheName != staticCacheName;
+				}).map(function(cacheName) {
+					return caches.delete(cacheName);
+				})
+			);
+		})
+	);
+})
 
-self.addEventListener('activate', function(e) {
-    console.log('service worker activated');
-
-    e.waitUntil(
-
-    
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(cacheNames.map(function(thisCacheName) {
-
-        if (thisCacheName !== cacheName) {
-
-          console.log('service worker removing rached files from cache - ', thisCacheName);
-          return caches.delete(thisCacheName);
-        }
-      }));
-    })
-  ); 
-
-});
-
-
-self.addEventListener('fetch', function(e) {
-  console.log('service worker Fetch', e.request.url);
-
- 
-  e.respondWith(
-
-  
-    caches.match(e.request)
-
-
-      .then(function(response) {
-
-       // if new cache found return it
-        if ( response ) {
-          console.log("service worker Found in Cache", e.request.url, response);
-         
-          return response;
-        }
-
-        // else fetch new
-
-        let requestClone = e.request.clone();
-        return fetch(requestClone)
-          .then(function(response) {
-
-            if ( !response ) {
-              console.log("service worker No response from fetch ")
-              return response;
-            }
-
-            let responseClone = response.clone();
-
-           
-            caches.open(cacheName).then(function(cache) {
-
-              
-              cache.put(e.request, responseClone);
-              console.log('service worker New Data Cached', e.request.url);
-
-             
-              return response;
-      
-                }); 
-
-          })
-          .catch(function(err) {
-            console.log('service worker Error Fetching & Caching New Data', err);
-          });
-
-
-      }) 
-  ); 
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.match(event.request)
+		.then(function(response) {
+			return response || fetch(event.request);
+		})
+	);
 });
